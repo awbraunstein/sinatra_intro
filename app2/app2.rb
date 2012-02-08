ee22979cc577e68694f3cf7ca84845ad3bb9e916
Andrew Braunstein <awbraunstein@gmail.com>
2012-02-08 15:01:53 -0500

    Added app2

--- app2/app2.rb
+++ app2/app2.rb
@@ -4,9 +4,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
+
 end
 
-get '/hello/:name' do
-  "Hello #{params[:name]}!"
-end
---
+++ app2/config.ru
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+require './app2'
+run Sinatra::Application
