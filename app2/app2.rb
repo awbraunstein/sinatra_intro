379361d1bbe2edd15577f508a623c9aa7ca4fa6a
Andrew Braunstein <awbraunstein@gmail.com>
2012-02-08 15:07:43 -0500

    added addition example

--- app2/app2.rb
+++ app2/app2.rb
@@ -4,6 +4,13 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-
+  redirect to('/home')
 end
 
+get '/home' do
+  "Hello"
+end
+
+get '/add/:first/:second' do
+  "#{(params[:first].to_i + params[:second].to_i).to_s}"
+end
